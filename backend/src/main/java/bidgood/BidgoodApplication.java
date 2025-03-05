c07ecbbe09c24241d3a4a0c0f3a5fe4b5cb45c98
kimdodo97 <eh4536@naver.com>
2025-03-05 21:30:22 +0900

    백엔드 프로젝트 설정

---
+++ backend/src/main/java/bidgood/BidgoodApplication.java
@@ -0,0 +1,13 @@
+package bidgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidgoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidgoodApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/bidgood/BidgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package bidgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidgoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
