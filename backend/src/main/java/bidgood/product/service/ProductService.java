492652e892acdd2bbaecd6fe5739a86231d73122
kimdodo97 <eh4536@naver.com>
2025-03-07 22:23:37 +0900

    feat : 상품 정보 단일 조회 로직 추가

--- backend/src/main/java/bidgood/product/service/ProductService.java
+++ backend/src/main/java/bidgood/product/service/ProductService.java
@@ -2,6 +2,8 @@ package bidgood.product.service;
 
 import bidgood.product.domain.Product;
 import bidgood.product.dto.req.ProductRegister;
+import bidgood.product.dto.res.ProductInfo;
+import bidgood.product.exception.ProductNotFound;
 import bidgood.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,5 +19,12 @@ public class ProductService {
     public Long createProduct(ProductRegister productRegister) {
         Product product = productRegister.toEntity();
         return productRepository.save(product).getId();
+    }
+
+    public ProductInfo get(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(ProductNotFound::new);
+
+        return ProductInfo.fromEntity(product);
     }
 }
