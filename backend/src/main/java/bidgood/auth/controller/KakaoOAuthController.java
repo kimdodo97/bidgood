e6306c138e9e15533d3803c9907484ef6e30590f
kimdodo97 <eh4536@naver.com>
2025-03-09 23:30:26 +0900

    feat : 사용자 생성 연결 기능 구현

--- backend/src/main/java/bidgood/auth/controller/KakaoOAuthController.java
+++ backend/src/main/java/bidgood/auth/controller/KakaoOAuthController.java
@@ -3,6 +3,9 @@ package bidgood.auth.controller;
 import bidgood.auth.dto.KakaoAuthRes;
 import bidgood.auth.dto.KakaoUserInfo;
 import bidgood.auth.service.OAuthService;
+import bidgood.user.domain.SocialType;
+import bidgood.user.dto.UserCreateReq;
+import bidgood.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class KakaoOAuthController {
     private final OAuthService oAuthService;
+    private final UserService userService;
 
     @GetMapping("oauth2/callback/kakao")
     public String kakaoLogin(String code){
         KakaoAuthRes oAuthToken = oAuthService.getOAuthToken(code);
         KakaoUserInfo userInfo = oAuthService.getUserInfo(oAuthToken.getAccessToken());
-        return "OK";
+
+        boolean duplicated = userService.duplicateUser(userInfo.getKakaoAccount().getEmail());
+        if(!duplicated){
+            userService.joinUser(userInfo.toRequest());
+            //로그인 화면 redirect
+            return "Main";
+        }
+
+        return "Login";
     }
 
 }
