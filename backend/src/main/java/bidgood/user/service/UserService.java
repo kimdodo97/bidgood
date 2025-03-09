a8d4461d304664f70be159231a2dc128e1b60746
kimdodo97 <eh4536@naver.com>
2025-03-09 23:29:35 +0900

    feat : 이메일 중복 체크 로직 추가

--- backend/src/main/java/bidgood/user/service/UserService.java
+++ backend/src/main/java/bidgood/user/service/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
 
         return newUser.getId();
     }
+
+    public boolean duplicateUser(String email){
+        return userRepository.existsByEmail(email);
+    }
 }
