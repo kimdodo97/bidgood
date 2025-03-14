f48f1005995a4ad44228f9f9c7e2fc3ff3db4d38
kimdodo97 <eh4536@naver.com>
2025-03-14 23:48:07 +0900

    feat : 스프링 시큐리티 Config 설정 추가

---
+++ backend/src/main/java/bidgood/global/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package bidgood.global.config;
+
+import bidgood.global.jwt.JwtProvider;
+import bidgood.global.jwt.filter.JwtAuthenticationFilter;
+import bidgood.global.util.UuidGenerator;
+import bidgood.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletRegistration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig implements WebMvcConfigurer {
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+    private final UuidGenerator uuidGenerator;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, ServletRegistrationBean h2Console) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .headers(header -> header.frameOptions((frame-> frame.disable())))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**", "/swagger-ui/**", "/health", "/v3/api-docs", "/swagger/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login","/logout", "/users/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(jwtProvider,userRepository,uuidGenerator);
+    }
+}
