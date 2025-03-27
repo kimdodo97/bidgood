47b2a759ddddd5715b65668ef6cd3e304c78a623
kimdodo97 <eh4536@naver.com>
2025-03-27 23:07:13 +0900

    feat : 사용자 인증 정보 리졸버 Web Config 추가

---
+++ backend/src/main/java/bidgood/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package bidgood.global.config;
+
+import bidgood.user.config.UserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final UserArgumentResolver userArgumentResolver;
+
+    public WebConfig(UserArgumentResolver userArgumentResolver) {
+        this.userArgumentResolver = userArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArgumentResolver);  // 사용자 ArgumentResolver 등록
+    }
+}
