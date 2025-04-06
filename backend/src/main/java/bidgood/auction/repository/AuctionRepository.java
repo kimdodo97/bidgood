4f8cd1c926e01493be0352ceb5c6fee5f579fb1b
kimdodo97 <eh4536@naver.com>
2025-04-06 20:01:47 +0900

    feat : 경매 리포지토리 인터페이스 구현

---
+++ backend/src/main/java/bidgood/auction/repository/AuctionRepository.java
@@ -0,0 +1,15 @@
+package bidgood.auction.repository;
+
+import bidgood.auction.domain.Auction;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuctionRepository extends JpaRepository<Auction, Long> {
+}
