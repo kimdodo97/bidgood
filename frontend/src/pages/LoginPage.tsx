7c678f97fc8a0ca0cb45898fff16c0cafc9945bf
kimdodo97 <eh4536@naver.com>
2025-04-10 23:50:10 +0900

    fix : 로그인 화면 수정

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import LogoImg from '../../assets/bidgood_logo.png'; // 이미지 경로는 프로젝트 구조에 따라 다름
-import KakaoImg from '../../assets/kakao_login.png';
+import LogoImg from '../assets/bidgood_logo.png'; // 이미지 경로는 프로젝트 구조에 따라 다름
+import KakaoImg from '../assets/kakao_login.png';
 
-
-interface ImportMetaEnv {
-    VITE_KAKAO_CLIENT_ID: string;
-    VITE_KAKAO_REDIRECT_URI: string;
-}
-
-interface LoginProps {
-    
-}
-
-const Login: React.FC<LoginProps> = () => {
+const Login: React.FC = () => {
     const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
     const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;    
-    
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-    
-    const handleKakaoLogin = () => {
-        
-    }
 
     return (
       <div className="w-full min-h-screen flex flex-col justify-between items-center bg-white px-4 py-10">
