d59b98936eef1d32edd1815e85bc050f39bc5aef
kimdodo97 <eh4536@naver.com>
2025-04-14 23:14:30 +0900

    fix : 라우팅 구조 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,23 +4,30 @@ import Login from './pages/LoginPage';
 import KakaoLogin from './components/login/KakaoLogin';
 import MainPage from './pages/MainPage';
 import BidPageWrapper from './pages/BIdPageWrapper';
+import Layout from './components/layout/Layout';
+import ItemLayout from './components/layout/ItemLayout';
 
 const App: React.FC = () => {
   return (
     <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route
+          path="/login/oauth2/callback/kakao" //redirect_url
+          element={<KakaoLogin />} //당신이 redirect_url에 맞춰 꾸밀 컴포넌트
+        />
+        <Route
           path="/login"
           element={
             <Login />
           }
         />
-        <Route
-          path="/login/oauth2/callback/kakao" //redirect_url
-          element={<KakaoLogin />} //당신이 redirect_url에 맞춰 꾸밀 컴포넌트
-        />
-        <Route path='/main' element={<MainPage/>}/>
-        <Route path="/auction/:auctionId" element={<BidPageWrapper />} />
+        <Route element={<Layout />}>
+          <Route path='/main' element={<MainPage/>}/>
+        </Route>
+        <Route element={<ItemLayout/>}>
+          <Route path="/auction/:auctionId" element={<BidPageWrapper />} />
+        </Route>
+        
       </Routes>
   );
 };
