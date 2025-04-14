de98d2bcde14ac97034ae2220e7c741c2f384e3c
kimdodo97 <eh4536@naver.com>
2025-04-14 23:12:18 +0900

    feat : 메인 이외 컴포넌트 헤더 추가

---
+++ frontend/src/components/common/ItemHeader.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import searchLogo from '../../assets/search.svg'
+import backIcon from '../../assets/back-icon.svg'
+
+const Header: React.FC = () => {
+    const handleBackClick = () => {
+        window.history.back();  // 뒤로 가기
+    };
+  return (
+    <div>
+        <div className="flex justify-between h-16 bg-white border-b border-D9D9D9">
+            <div className='w-full m-5 text-xl font-bold'>
+            <img 
+                src={backIcon} 
+                alt="뒤로 가기" 
+                onClick={handleBackClick}  // 클릭 시 뒤로가기
+                className="cursor-pointer"
+            />
+            </div>
+            <div className="flex w-full justify-end">
+                <img
+                    alt="종 모양 아이콘"
+                    src={searchLogo}
+                    className="w-10 h-10 m-2 cursor-pointer"
+                />
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Header;
