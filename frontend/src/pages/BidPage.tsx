fa07394c9a84ae8411108d676f030ffa53bc3b1f
kimdodo97 <eh4536@naver.com>
2025-04-14 23:14:08 +0900

    feat : 경매품 이미지 string 변경

--- frontend/src/pages/BidPage.tsx
+++ frontend/src/pages/BidPage.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import BiddingButton from '../components/bid/BiddingButton';
 import BiddingInfo from '../components/bid/BiddingInfo';
 import ProductInfo from '../components/bid/ProductInfo';
-import logo from '../assets/bidgood_logo.png';
+import logo1 from '../assets/bidgood_logo.png';
+import logo2 from '../assets/bidgood_logo.png';
+import logo3 from '../assets/bidgood_logo.png';
 
 interface BidPageProps {
     auctionId : number,
@@ -11,9 +13,9 @@ interface BidPageProps {
 
 const BidPage: React.FC<BidPageProps> = ({ auctionId, onMessage }) => {
     return (
-        <div className='flex-col justify-center p-4 mt-10'>
-            <ProductInfo imageUrl={logo} productName={'테스트상품'} productDesciption={'해당 상품은 테스트 상품입니다.'} category={'건담'}/>
-            <div className="fixed bottom-0 left-0 w-fullbackdrop-blur-sm shadow-md p-5 z-50 w-full">
+        <div className='flex-col justify-center p-4'>
+            <ProductInfo imageUrl={[logo1,logo2,logo3]} productName={'테스트상품'} productDesciption={'해당 상품은 테스트 상품입니다.'} category={'건담'}/>
+            <div className="fixed bottom-15 left-0 backdrop-blur-sm p-5 z-50 w-full">
                 <BiddingInfo
                     biddingUserCount={10}
                     maxBidUserid={2}
