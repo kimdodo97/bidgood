a7efd83914cc68504c61aee8589893b9bf09d318
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2025-04-14 23:33:24 +0900

    [FE] 경매품 조회/경매화면 컴포넌트 구현 (#21)
    * init : 프론트 프로젝트 재설정
    
    * 프로젝트 재설정 프론트
    
    * chroe : 프론트엔드 기본 설정
    
    * feat : 리액트 라우터 적용
    
    * design : 테일윈드  css 적용
    
    * assets : 기본 아이콘 추가
    
    * feat : 리액트 레이아웃 사용
    
    * feat : 로그인 컴포넌트 구성 추가
    
    * chroe : axios 추가
    
    * feat : 개발 모드 제거
    
    * feat : 레이아웃 Outlet 형태 변경
    
    * fix : 로그인 화면 수정
    
    * feat : 카카오 로그인 컴포넌트 추가
    
    * feat : 카카오 로그인 커스텀 훅
    
    * feat : 경매 상품 목록 컴포넌트
    
    * feat : 경매 입찰 화면 컴포넌트 디자인 추가
    
    * feat : 경매화면 페이지 추가
    
    * feat : 메인 페이지 추가
    
    * feat : 경로 라우팅 추가
    
    * prop : oauth 토큰 값 변경 수정
    
    * chroe : 스와이퍼 의존성 추가
    
    * asset : 기본아이콘 추가
    
    * feat : 상품 이미지 swiper 추가
    
    * css : swiper 버튼 css 추가
    
    * feat : 메인화면 header/footer 추가
    
    * feat : 메인 이외 컴포넌트 헤더 추가
    
    * fix : 일반 레이아웃  경로 변경
    
    * feat : 메인 이외 컴포넌트용 레이아웃 추가
    
    * feat : 경매품 이미지 string 변경
    
    * fix : 메인 페이지 div 제거
    
    * fix : 라우팅 구조 변경
    
    * remove ; 레이아웃 경로 변경 삭ㅈ[
    
    * fix : 컴포넌트 이름 수정
    
    * fix : 아이템레이아웃 컴포넌트 명 수정
    
    * fix : productDescription 오탈자 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AuctionCard from '../components/auction/AuctionCard';
 import AuctionList from '../components/auction/AuctionList';
 
 interface MainPageProps {
@@ -9,11 +8,7 @@ interface MainPageProps {
 const MainPage: React.FC<MainPageProps> = () => {
     return (
         <div>
-            <div></div>
-            <div>
             <AuctionList/>
-              </div>
-            <div></div>
         </div>
     );
 };
