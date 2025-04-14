88fa1862455f431897f559affc003671630bd1e0
kimdodo97 <eh4536@naver.com>
2025-04-14 23:11:18 +0900

    feat : 상품 이미지 swiper 추가

--- frontend/src/components/bid/ProductInfo.tsx
+++ frontend/src/components/bid/ProductInfo.tsx
@@ -1,25 +1,56 @@
+// components/ProductInfo.tsx
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import './css/BidImage.css'
+
 
 interface ProductInfoProps {
-    imageUrl : string,
-    productName: string,
-    productDesciption : string,
-    category: string
-}
-
-const ProductInfo: React.FC<ProductInfoProps> = ({imageUrl, productName, productDesciption, category}) => {
+    imageUrl: string[];
+    productName: string;
+    productDesciption: string;
+    category: string;
+  }
+  
+  const ProductInfo: React.FC<ProductInfoProps> = ({
+    imageUrl,
+    productName,
+    productDesciption,
+    category,
+  }) => {
     return (
-        <div className='justify-center'>
-            <div className='w-full flex justify-center'>
-                <img src={imageUrl} className="w-[350px] h-[350px] object-cover ml-2" />
-            </div>
-            <div className='flex flex-col p-3 gap-2'>
-                <div className='font-bold text-[20px]'>{productName}</div>
-                <div className='text-[14px] text-gray-500'>{category}</div>
-                <div className='text-[16px] mt-2'>{productDesciption}</div>
-            </div>
+      <div className="justify-center">
+        <div className="w-full flex justify-center items-center">
+          <Swiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{
+              clickable: true,
+            }}
+            className="w-[350px] h-[350px] rounded overflow-hidden"
+          >
+            {imageUrl.map((url, idx) => (
+              <SwiperSlide key={idx}>
+                <img
+                  src={url}
+                  className="w-full h-full object-cover"
+                  alt={`product-${idx}`}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
+        <div className="flex flex-col p-3 gap-2">
+          <div className="font-bold text-[20px]">{productName}</div>
+          <div className="text-[14px] text-gray-500">{category}</div>
+          <div className="text-[16px] mt-2">{productDesciption}</div>
+        </div>
+      </div>
     );
-};
-
-export default ProductInfo;
+  };
+  
+  export default ProductInfo;
